384f800cf412692952920c0fe4e89cf3c29fb049
Max <maxguojiaqi@gmail.com>
2020-06-08 18:48:04 -0400

    ''

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const db = require("../models");
+const db = require("../models/index");
 const { ObjectId } = require('mongodb')
 
 // Handle getting all the workouts 
